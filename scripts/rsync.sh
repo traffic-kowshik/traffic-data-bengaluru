ee52161d0afb63c2a84c1001f6ace3ec3da36d5d
Traffic Kowshik <traffic.kowshik@gmail.com>
2025-09-23 06:39:45 +0530

    Update rsync script to include logging and project directory variables
    - Added project and log directory variables to `rsync.sh` for improved readability and maintainability.
    - Redirected output of the rsync command to a log file for better tracking of synchronization processes.

--- scripts/rsync.sh
+++ scripts/rsync.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+project_dir=/home/traffic_kowshik/code/traffic-kowshik/traffic-data-bengaluru
+log_dir=$project_dir/logs
+
 /usr/bin/gsutil -m rsync -r \
   "$HOME/code/traffic-kowshik/traffic-data-bengaluru/data/" \
-  gs://traffic-kowshik-bmtc/traffic-data-bengaluru/data/
+  gs://traffic-kowshik-bmtc/traffic-data-bengaluru/data/ >> $log_dir/rsync.log 2>&1
