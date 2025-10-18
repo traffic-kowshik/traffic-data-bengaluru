2d1b685d08964a8c203e218bd23677c92b319a92
Traffic Kowshik <traffic.kowshik@gmail.com>
2025-10-18 22:49:28 +0530

    Add About page to Streamlit application and update navigation
    - Created a new `about.py` file for the About page in the Streamlit app.
    - Updated `main.py` to include the About page in the navigation structure.

---
+++ app/about.py
@@ -0,0 +1,9 @@
+import os
+import requests
+import streamlit as st
+import pandas as pd
+from dotenv import load_dotenv
+load_dotenv()
+
+st.title("🚦 About")
+st.set_page_config(page_title="Traffic Data Bengaluru", layout="wide")
--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 import streamlit as st
 
 pages = {
+    "Traffic Data Bengaluru": [
+        st.Page('about.py', title="About")
+    ],
     "BMTC": [
-        st.Page("bmtc/routes.py", title="Routes")
+        st.Page("bmtc/routes.py", title="Routes", url_path="bmtc-routes")
     ],
     "Namma Metro": [
-        st.Page("namma_metro/ridership.py", title="Ridership")
+        st.Page("namma_metro/ridership.py", title="Ridership", url_path="namma-metro-ridership")
     ],
 }
 
